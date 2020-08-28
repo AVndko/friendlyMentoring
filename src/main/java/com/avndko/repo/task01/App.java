d3dcb5bb6db69dfd5b2afb0b9a463ad94b9ac804
AVNdko <nantonv@bk.ru>
2020-08-28 16:15:30 +0300

    .gitignore update add ConsoleCalc

---
+++ src/main/java/com/avndko/repo/task01/App.java
@@ -0,0 +1,10 @@
+package main.java.com.avndko.repo.task01;
+
+import main.java.com.avndko.repo.task01.core.ConsoleCalc;
+
+public class App {
+    public static void main(String[] args) {
+        int result= new ConsoleCalc().calculate();
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/com/avndko/repo/task01/core/ConsoleCalc.java
@@ -0,0 +1,42 @@
+package main.java.com.avndko.repo.task01.core;
+
+import java.util.Scanner;
+
+public class ConsoleCalc {
+
+    public static int result;
+
+    public static int calculate(){
+        Scanner scanner= new Scanner(System.in);
+
+        System.out.print("Enter the first integer: ");
+        int a= scanner.nextInt();
+
+        System.out.print("Enter operation (+,-,*,/,%): ");
+        char operation= scanner.next().charAt(0);
+
+        System.out.print("Enter the second integer: ");
+        int b= scanner.nextInt();
+
+        switch (operation){
+            case '+':
+                result= a+b;
+                break;
+            case '-':
+                result= a-b;
+                break;
+            case '*':
+                result= a*b;
+                break;
+            case '/':
+                result= a/b;
+                break;
+            case '%':
+                result= a%b;
+                break;
+            default:
+                System.out.println("Operation error!");
+        }
+        return result;
+    }
+}
